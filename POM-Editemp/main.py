f700239ac138aff14167eeafc2d1875b7c2ddb41
Kruthi01 <156498124+Kruthi01@users.noreply.github.com>
2024-06-11 20:37:02 +0530

    Add files via upload

---
+++ POM-Editemp/main.py
@@ -0,0 +1,51 @@
+from Data import data
+from Locator import locator
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+
+class LoginPage:
+
+
+   def __init__(self):
+       self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+   def boot(self):
+       self.driver.get(data.WebData().url)
+       self.driver.maximize_window()
+       self.driver.implicitly_wait(10)
+
+   def quit(self):
+       self.driver.quit()
+
+   def login(self):
+       try:
+           self.boot()
+           locator.WebLocators().enterText(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
+           locator.WebLocators().enterText(self.driver, locator.WebLocators().passwordLocator, data.WebData().password)
+           locator.WebLocators().clickButton(self.driver, locator.WebLocators().buttonLocator)
+           self.driver.implicitly_wait(10)
+           locator.WebLocators().clickButton(self.driver,locator.WebLocators().buttonLocator1)
+           self.driver.implicitly_wait(10)
+           locator.WebLocators().enterText(self.driver,locator.WebLocators().empnameLocator,data.WebData().empname)
+           self.driver.implicitly_wait(10)
+           locator.WebLocators().clickButton(self.driver,locator.WebLocators().searchbuttonLocator)
+           self.driver.implicitly_wait(10)
+           locator.WebLocators().clickButton(self.driver,locator.WebLocators().editbuttonLocator)
+           self.driver.implicitly_wait(10)
+           locator.WebLocators().enterText(self.driver,locator.WebLocators().middlenameLocator,data.WebData().middlename)
+           self.driver.implicitly_wait(10)
+           locator.WebLocators().clickButton(self.driver,locator.WebLocators().savebutton2Locator)
+           self.driver.implicitly_wait(10)
+           print("Success !!")
+
+
+       except NoSuchElementException as e:
+           print("Error!")
+       finally:
+           self.quit()
+
+
+obj = LoginPage()
+obj.login()
